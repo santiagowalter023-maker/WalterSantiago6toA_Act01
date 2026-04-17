c7bf5af14877bcd538f382bf7d25fe383c579f4c
Walter Santiago <santiagowalter023@gmail.com>
2026-04-16 23:08:16 -0300

    07 TERMINADO

--- Ejercicios de Poo/07.py
+++ Ejercicios de Poo/07.py
@@ -6,3 +6,51 @@
 #interés (cantidad*interés/100) y otro método para mostrar la información, datos del titular plazo, interés y total
 #de interés.
 #Crear al menos un objeto de cada subclase.
+
+class Cuenta:
+    def __init__(self,titlr,cant):
+        self.titular  = titlr
+        self.cantidad = cant
+
+    def mostrasdatos(self):
+        print("El titular  es :",self.titular)
+        print("La cantidad es :",self.cantidad)
+
+class CajaAhorro(Cuenta):
+    def __init__(self,titlr,cant):
+        super().__init__(titlr, cant)
+
+    def datosmostar(self):
+        self.mostrasdatos()
+
+class PlazoFijo(Cuenta):
+    def __init__(self,titlr,cant,plaz,intrs):
+        super().__init__(titlr,cant)
+        self.interes = intrs
+        self.plazo   = plaz
+
+    def calculintrs(self):
+        return (self.cantidad * self.interes / 100)
+    
+    def datos(self):
+        self.mostrasdatos()
+        print("Interest   :",self.interes)
+        print("Plazo Fijo :",self.plazo)
+        print("Total es   :",self.calculintrs())
+
+fondo = CajaAhorro("Messi", 40000)
+fondo.datosmostar()
+
+print("-" * 20)
+
+plazo = PlazoFijo("Cristiano", 50000, 30, 10)
+plazo.datos()
+
+         
+
+
+     
+
+
+
+
