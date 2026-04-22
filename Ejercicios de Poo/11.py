eb3e953986ef9ce876b5fd3f248eff5a50efa902
Walter Santiago <santiagowalter023@gmail.com>
2026-04-21 21:17:59 -0300

    subida 11

---
+++ Ejercicios de Poo/11.py
@@ -0,0 +1,6 @@
+#Ejercicio 11: BATALLA DE HECHICEROS
+#Dos hechiceros se enfrentan en un duelo mágico. Uno será controlado por el usuario, quien podrá tomar
+#decisiones estratégicas durante el combate.
+#Cada acción tiene consecuencias sobre los recursos disponibles del personaje, por lo que será necesario
+#administrarlos correctamente.
+#El enfrentamiento continúa hasta que uno de los participantes ya no pueda seguir luchando.
