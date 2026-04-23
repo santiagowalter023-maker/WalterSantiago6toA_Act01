827a5e99263a59753723b4efc703fde69c20e4b4
Walter Santiago <santiagowalter023@gmail.com>
2026-04-22 21:02:25 -0300

    Subida 13

---
+++ Ejercicios de Poo/13.py
@@ -0,0 +1,9 @@
+#Ejercicio 13: DESAFÍO DE DADOS
+#En un juego de azar participan varios jugadores. Uno de ellos será controlado por el usuario. En cada ronda,
+#el usuario podrá lanzar un dado para obtener un valor que se acumulará en su puntaje total. Los demás
+#jugadores también participarán automáticamente.
+#Al finalizar varias rondas, se deberá determinar el resultado del juego.
+
+import random
+
+
