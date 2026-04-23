9293b801737c4d8e19a98c64cd7b6dd4f546f8a9
Walter Santiago <santiagowalter023@gmail.com>
2026-04-23 19:58:31 -0300

    subida del 14

---
+++ Ejercicios de Poo/14.py
@@ -0,0 +1,5 @@
+#Ejercicio 14: ÚLTIMA DEFENSA
+#Una torre protege un territorio de ataques constantes. El usuario podrá decidir cómo actuar frente a cada
+#situación. A medida que transcurre el tiempo, la torre recibe daños que afectan su estado. El sistema debe
+#permitir observar su condición en cada momento.
+#El juego finaliza cuando la torre ya no puede resistir más ataques.
