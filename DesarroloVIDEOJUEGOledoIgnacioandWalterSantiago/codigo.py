4dfcd8489f8ff369129532e126e0e86691596738
Walter Santiago <santiagowalter023@gmail.com>
2026-05-28 22:57:56 -0300

    actualixzacion

--- DesarroloVIDEOJUEGOledoIgnacioandWalterSantiago/codigo.py
+++ DesarroloVIDEOJUEGOledoIgnacioandWalterSantiago/codigo.py
@@ -1,4 +1,19 @@
 # ACA VA ESTAR EL CODIGO PICHON
 import pygame
 
+pygame.init()
+
+ancho = 1020
+alto = 720
+
+ventana = pygame.display.set_mode((ancho, alto))
+
+corriendo = True 
+while corriendo == True:
+    for evento in pygame.event.get():
+        if evento.type == pygame.QUIT:
+            corriendo = False
+
+pygame.quit()
+
 print("Bienvenido")
