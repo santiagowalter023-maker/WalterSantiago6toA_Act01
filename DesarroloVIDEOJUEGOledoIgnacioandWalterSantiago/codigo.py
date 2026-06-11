70ad42fbca410b77fe7774d39177a46dd47448dd
Walter Santiago <santiagowalter023@gmail.com>
2026-06-10 21:43:02 -0300

    cambios

--- DesarroloVIDEOJUEGOledoIgnacioandWalterSantiago/codigo.py
+++ DesarroloVIDEOJUEGOledoIgnacioandWalterSantiago/codigo.py
@@ -1,19 +1,43 @@
+#python "e:/Programacion 3/01Heladeria WalterSantiago/Python-Reposity/DesarroloVIDEOJUEGOledoIgnacioandWalterSantiago/codigo.py"
 # ACA VA ESTAR EL CODIGO PICHON
-import pygame
+import arcade
+import codigosb
 
-pygame.init()
+# ACA VA ESTAR EL CODIGO PICHON
 
-ancho = 1020
-alto = 720
+window = arcade.open_window(codigosb.ALTO, codigosb.ANCHO, "REMINENCE OF GRACIA")
+window.center_window()
 
-ventana = pygame.display.set_mode((ancho, alto))
-
-corriendo = True 
-while corriendo == True:
-    for evento in pygame.event.get():
-        if evento.type == pygame.QUIT:
-            corriendo = False
-
-pygame.quit()
+class GameView(arcade.View):
+    def __init__(self) -> None:
+        super().__init__()
+        
+    def on_draw(self) -> None:
+        self.clear()
+        
+        #ACA VAMOS A RENDERIZAR
+        
+        #circulos
+        arcade.draw_circle_filled(center_x=100, center_y=100, radius=30, color=(255, 0, 0))
+        arcade.draw_circle_outline(center_x=150, center_y=150, radius=30, color=(255, 0, 0))
+        #rectangulos
+        arcade.draw_xywh_rectangle_filled(250, 250, 50, 90, (255, 0, 0))
+        
+        arcade.draw_xywh_rectangle_filled(250, 200, 50, 90, (255, 0, 0))
+        
+        arcade.draw_xywh_rectangle_outline(250, 50, 50, 80, (0, 0, 255))
+        #arcos
+        arcade.draw_arc_filled(500, 300, 100, 100, (0, 255, 0), start_angle=0, end_angle=100)
+        
+        arcade.draw_arc_outline(550, 350, 100, 100, color=(0, 255, 0), start_angle= 0, end_angle= 90)
+        #parabolas
+        
+        #lineas
+        
+        
+        
+juego = GameView()
+window.show_view(juego)
+arcade.run()
 
 print("Bienvenido")
